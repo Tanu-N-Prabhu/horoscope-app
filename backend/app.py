a5ced6baff88be1a3d7d1618137ff436fedc336e
Tanu-N-Prabhu <tanunprabhu95@gmail.com>
2025-03-26 00:04:37 -0600

    Updated the requirements file

--- backend/app.py
+++ backend/app.py
@@ -70,5 +70,5 @@ def store_feedback():
 
     return jsonify({"message": "Feedback received successfully!"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
